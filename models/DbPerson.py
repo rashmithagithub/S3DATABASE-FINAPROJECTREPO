eb2a1bc029983bd61b9685ed4a0dac5db3404266
harishdonepudi <harishdonepudi>
2022-05-04 06:04:32 +0000

    empdata

--- models/DbPerson.py
+++ models/DbPerson.py
@@ -1,22 +1,28 @@
 from config import db
 
 class DbPerson(db.Model):
-    __tablename__='people'
-    sno=db.Column(db.Integer,primary_key=True)
+    __tablename__='employee'
+    eno=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(30),index=False,unique=False,nullable=False)
     city=db.Column(db.String(30),index=False,unique=False,nullable=False)
+    designation=db.Column(db.String(30),index=False,unique=False,nullable=False)
+    age=db.Column(db.integer(30),index=False,unique=False,nullable=False)
     
 
-    def __init__(self,sno,name,city):
-        self.sno=sno
+    def __init__(self,eno,name,city,designation,age):
+        self.eno=eno
         self.name=name
         self.city=city
+        self.designation=designation
+        self.age=age
     
     def serialize(self):
         return {
-            'sno':self.sno,
+            'eno':self.eno,
             'name':self.name,
-            'city':self.city
+            'city':self.city,
+            'designation':self.designation,
+            'age':self.age
             }
     
     def __repr__(self):
--- routes/dbroutes.py
+++ routes/dbroutes.py
@@ -2,21 +2,23 @@ from config import app,db
 from flask import jsonify,request,abort
 from models import DbPerson
 
-@app.route("/dbpeople")
+@app.route("/employees")
 def getDbPeople():
     listp=DbPerson.query.all()
     result = [x.serialize() for x in listp]
     return jsonify(result)
 
 
-@app.route("/dbpeople",methods=['POST'])
+@app.route("/employees",methods=['POST'])
 def processDepartments():
     try:
         input=request.get_json()
-        sno=input['sno']
+        eno=input['eno']
         name=input['name']
         city=input['city']
-        db.session.add(DbPerson(sno,name,city))
+        designation=input['designation']
+        age=input['age']
+        db.session.add(DbPerson(eno,name,city,designation,age))
         db.session.commit()
         return {"status": "success"}, 201
     except:
